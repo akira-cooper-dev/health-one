15ed93326a88453c37029d321b543b14fffc9efa
akira-cooper-dev <nagaokakid@gmail.com>
2026-04-12 20:46:43 -0500

    Remove models and services not generated from openapi spec.

--- frontend/src/app/models/dto/create-user-exercise-dto.ts
+++
@@ -1,7 +0,0 @@
-
-export interface CreateUserExerciseDto {
-    userId: number;
-    exerciseApiId: string; // The exerciseId from the Exercise DB API
-    exerciseName: string;
-    time?: string;  // length of exercise in hh:mm:ss format (optional)
-}
--- frontend/src/app/models/dto/exercise-db-api/base-exercise-request-dto.ts
+++
@@ -1,7 +0,0 @@
-export interface BaseExerciseRequestDto {
-    offset?: number; // The number of exercises to skip from the start of the list. Useful for pagination to fetch subsequent pages of results.
-    limit?: number; // min = 1, max = 25 --> The maximum number of exercises to return in the response. Limits the number of results for pagination purposes.
-}
-
-export type SortBy = 'name' | 'exerciseId' | 'targetMuscles' | 'bodyParts' | 'equipments';
-export type SortOrder = 'asc' | 'desc';
